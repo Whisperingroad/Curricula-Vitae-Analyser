22dc99c0399dea3aaff9d9b097ec95d9e39c8cb3
Whisperingroad <sebastian.wong@hotmail.com>
2015-10-09 18:16:23 +0800

    updated library

---
+++ Curricula-Vitae-Analyzer/src/controller/Controller.java
@@ -0,0 +1,18 @@
+//package controller;
+
+//import extractor.PDFTextParser;
+//import storage.Storage;
+//import utils.Constants;
+
+//public class Controller {
+	
+	//protected PDFTextParser pdfTextParserObj = new PDFTextParser();
+	//protected Parser cvParser = new Parser();
+	//protected Storage storage = new Storage()
+		//	;
+	
+	
+	
+	
+
+//}
